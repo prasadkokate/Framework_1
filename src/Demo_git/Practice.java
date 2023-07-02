671782271aff2adf75c87d93ee14f22a26b36014
prasadkokate <prasad4kokate@gmail.com>
2023-07-02 17:56:02 +0530

    First commit

---
+++ src/Demo_git/Practice.java
@@ -0,0 +1,10 @@
+package Demo_git;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		System.out.println("First comment");
+
+	}
+
+}
---
+++ src/Demo_git/Practice_1.java
@@ -0,0 +1,10 @@
+package Demo_git;
+
+public class Practice_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Second comment");
+
+	}
+
+}
