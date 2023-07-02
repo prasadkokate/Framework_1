92c3ec1950f84a8d9cd8d7f2c126b9dec2078fde
prasadkokate <prasad4kokate@gmail.com>
2023-07-02 18:47:18 +0530

    Added 4 class

--- src/Demo_git/Practice4.java
+++ src/Demo_git/Practice4.java
@@ -1,9 +1,9 @@
 package Demo_git;
 
-public class Practice3 {
+public class Practice4 {
 
 	public static void main(String[] args) {
-		System.out.println("Third comment");
+		System.out.println("Fourth comment");
 
 	}
 
